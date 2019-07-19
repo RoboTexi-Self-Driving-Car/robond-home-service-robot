33ef8fd2d1764077d703d1b599731424eeee144d
Peng Xu <robotpengxu@gmail.com>
2019-07-19 11:32:16 -0700

    update map & use turtlebot_simulator as local

--- wall_follower/src/wall_follower.cpp
+++ wall_follower/src/wall_follower.cpp
@@ -43,35 +43,35 @@ bool robot_move(const ROBOT_MOVEMENT move_type)
     else if (move_type == FORWARD) {
         ROS_INFO("[ROBOT] Always FORWARD! \n");
         motor_command.angular.z = 0.0;
-        motor_command.linear.x = 0.5;
+        motor_command.linear.x = 0.1;
     }
 
     else if (move_type == BACKWARD) {
         ROS_INFO("[ROBOT] I'm going back! \n");
-        motor_command.linear.x = -0.75;
+        motor_command.linear.x = -0.25;
         motor_command.angular.z = 0.0;
     }
 
     else if (move_type == TURN_LEFT) {
         ROS_INFO("[ROBOT] I'm turning left! \n");
         motor_command.linear.x = 0.0;
-        motor_command.angular.z = 1.0;
+        motor_command.angular.z = 0.2;
     }
 
     else if (move_type == TURN_RIGHT) {
         ROS_INFO("[ROBOT] I'm turning right! \n");
         motor_command.linear.x = 0.0;
-        motor_command.angular.z = -1.0;
+        motor_command.angular.z = -0.2;
     }
     else if (move_type == GO_RIGHT) {
         ROS_INFO("[ROBOT] I'm goin right! \n");
-        motor_command.linear.x = 0.25;
-        motor_command.angular.z = -0.25;
+        motor_command.linear.x = 0.05;
+        motor_command.angular.z = -0.05;
     }
     else if (move_type == GO_LEFT) {
         ROS_INFO("[ROBOT] I'm goin left! \n");
-        motor_command.linear.x = 0.25;
-        motor_command.angular.z = 0.25;
+        motor_command.linear.x = 0.05;
+        motor_command.angular.z = 0.05;
     }
     else {
         ROS_INFO("[ROBOT_MOVE] Move type wrong! \n");
